b1351a4af0e5bf7d6eff7f24d5da46a5e36c09d0
TextError <savinraul1983@yahoo.com>
2019-09-12 16:42:04 +0100

    routes & pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Navbar from './layout/navbar/Navbar';
+import Home from './pages/home/Home';
+import SignIn from './pages/sign_In/SignIn';
+import Register from './pages/register/Register';
+import Footer from './layout/footer/Footer';
+import NotFound from './pages/not_Found/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      app
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/sign-in' component={SignIn} />
+          <Route exact path='/register' component={Register} />
+
+          <Route path='*' component={NotFound} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
