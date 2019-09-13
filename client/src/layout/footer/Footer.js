8bc1b07090394d58df61e0fb8264895585dfab51
TextError <savinraul1983@yahoo.com>
2019-09-13 17:07:18 +0100

    footer

--- client/src/layout/footer/Footer.js
+++ client/src/layout/footer/Footer.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const Footer = () => (
-  <div>Footer</div>
-)
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer bg-dark text-white'>
+      <div className='row no-gutters'>
+        <div className='col-sm-6'>
+          <div className='footer-text'>
+            <span>Copyright &copy; {new Date().getFullYear()}</span>
+          </div>
+        </div>
+        <div className='col-sm-6'>
+          <div className='footer-text'>
+            <span>Made with <i className="fas fa-heart"></i> by Savin Raul-Calin</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default Footer;
