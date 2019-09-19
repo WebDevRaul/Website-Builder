3bd6f3fb138083581e638303e1428760890212f5
TextError <savinraul1983@yahoo.com>
2019-09-19 14:58:47 +0100

    Textarea npm

--- client/src/components/dashboard/Index_Dashboard.jsx
+++ client/src/components/dashboard/Index_Dashboard.jsx
@@ -1,28 +1,30 @@
 import React, { useState } from 'react';
+import Textarea from 'react-textarea-autosize';
+import classnames from 'classnames';
+import isEmpty from '../utils/isEmpty/isEmpty';
 
 import Responsive from '../common/authWrapper/Responsive';
 import Title from '../common/title/Title';
 import CustomButton from '../common/button/Custom_Button';
-import TextArea from '../common/textarea/TextArea';
 
 import './indexDashboard.scss';
 
 const IndexDashboard = () => {
   const [error, setErrors] = useState('');
-  const [textarea, setTextarea] = useState('');
+  const [textarea, setTextarea] = useState(undefined);
 
   const onFocus = () => {
 
-  }
+  };
 
   const onChange = e => {
     setTextarea(e.target.value)
-  }
+  };
 
-  const onSubmit = e => {
+  const onClick = e => {
     e.preventDefault();
-    setErrors('');
-  }
+    console.log(textarea)
+  };
 
   return (
     <div className='dashboard'>
@@ -31,9 +33,12 @@ const IndexDashboard = () => {
         <Title title='Profile' />
         <p>name</p>
         <p>Email</p>
-        <form noValidate className='form-message'>
-          <TextArea name='message' value={textarea} onChange={onChange} onFocus={onFocus} label='enter message' error={error} rows='5' />
-          <div className='submit' onSubmit={onSubmit}>
+        <form className='form-message'>
+          <div className='textarea'>
+            <Textarea minRows={3} name='textarea' onChange={e => onChange(e)}/>
+            <span className={classnames('textarea-label', {'shrink': !isEmpty(textarea)})}>enter message</span>
+          </div>
+          <div className='submit' onClick={onClick}>
             <CustomButton value='submit' isClass='inverted' />
           </div>
         </form>
